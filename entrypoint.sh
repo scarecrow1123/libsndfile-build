141f898962fef97b92da5db5950cdf47fa9e893a
ananda seelan <ananda.seelan@gmail.com>
2020-08-23 15:55:57 +0530

    Use latest release of libsndfile

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 mkdir libsndfile_build
-git clone https://github.com/erikd/libsndfile
+git clone --depth 1 --branch v1.0.29 https://github.com/erikd/libsndfile
 cd libsndfile
 ./autogen.sh
 ./configure --enable-werror --prefix=/usr/libsndfile/libsndfile_build
